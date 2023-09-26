91b5fb41220d4f0b5964e7d3643d230d11e5c436
suria-raul <stadnitzki.nero.nero@gmail.com>
2023-09-26 20:17:15 +0800

    PopOver sheesh

--- resources/js/Layout/App.vue
+++ resources/js/Layout/App.vue
@@ -1,8 +1,38 @@
+<script>
+import PopOver from "./PopOver.vue"
+
+export default {
+    components: {
+        PopOver
+    },
+    data() {
+        return {
+            show: false,
+            dataOne: '',
+            dataTwo: ''
+        }
+    },
+    methods: {
+        theHover(dataOne, dataTwo, show) {
+            this.dataOne = dataOne
+            this.dataTwo = dataTwo
+            this.show = show
+        }
+    }
+}
+</script>
 <template>
     <h1>
         Laravel Vue Vite!
     </h1>
+    <PopOver :show="show" :data-one="dataOne" :data-two="dataTwo"></PopOver>
     <div class="container mx-auto">
+        <span
+            @mouseover="theHover('Hovered Yeah!', 'Second Data!', true)"
+            @mouseleave="theHover('', '', false)"
+        >
+            Randomicity
+        </span>
         <router-view />
     </div>
 </template>
